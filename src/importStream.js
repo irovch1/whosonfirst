22f908810b17fb0205c5492779ea8ff32f54f468
Ivan Razhnou <ivan.razhnou@gmail.com>
2021-04-15 00:52:16 +0300

    Add dump

--- src/importStream.js
+++ src/importStream.js
@@ -1,6 +1,19 @@
 const peliasModel = require('pelias-model');
 const blacklistStream = require('pelias-blacklist-stream');
 
+const through = require('through2');
+const DUMP_TO = process.env.DUMP_TO;
+
+function createDocumentMapperStream() {
+  if(DUMP_TO) {
+    return through.obj( function( model, enc, next ){
+      next(null, model.callPostProcessingScripts());
+    });
+  }
+
+  return peliasModel.createDocumentMapperStream();
+}
+
 /*
   This function performs the import based on the collection of WOF records
   gathered so far.  The parameters are:
@@ -10,11 +23,12 @@ const blacklistStream = require('pelias-blacklist-stream');
   destination_pipe: where the Pelias Document instances end up
 
 */
+
 function fullImport(wof_record_stream, document_generator, destination_pipe, callback) {
   wof_record_stream
     .pipe(document_generator)
     .pipe(blacklistStream())
-    .pipe(peliasModel.createDocumentMapperStream())
+    .pipe(createDocumentMapperStream())
     .pipe(destination_pipe)
     .on('finish', callback);
 
